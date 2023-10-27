0aafb35287cd733a4c73108561c619d1bdb59dd9
Chandler <chandlerqvan@gmail.com>
2023-10-26 19:09:47 -0700

    Added Directory opening and text file editing

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { readTextFile, writeTextFile } from '@tauri-apps/api/fs';
 import './App.css';
 import Sidebar from './components/Sidebar';
 import { useEffect, useState } from 'react';
@@ -6,23 +7,43 @@ import { useEffect, useState } from 'react';
 function App() {
 
   const [text_value, setText] = useState<string>("");
+  const [cur_file, setCurFile] = useState<string>("");
 
   useEffect(() => {
-
+    function onKeyDown(e : KeyboardEvent){
+      if(e.ctrlKey && e.key === 's'){
+        onSave();
+      }
+    }
     document.addEventListener('keydown', onKeyDown, false)
 
-  }, []);
+    return () => {
+      document.removeEventListener('keydown', onKeyDown);
+    };
 
-  function onKeyDown(e : KeyboardEvent){
-    if(e.ctrlKey && e.key === 's'){
-      console.log("save");
-    }
+  }, [cur_file, text_value]);
+
+  
+
+  async function onSave(){
+    console.log(cur_file);
+
+    if(!cur_file || cur_file === '') return;
+
+    await writeTextFile(cur_file, text_value);
+  }
+
+  async function onFileSelect(file : string, dir : string){
+    const new_file = dir + '\\' +file
+
+    setCurFile(new_file);
+    setText(await readTextFile(new_file))
   }
 
   return (
     <div className='flex flex-row' >
      
-      <Sidebar />
+      <Sidebar onFileSelect={(f: string, dir: string) => {onFileSelect(f, dir)}} onSave={onSave}/>
 
       <textarea className='editor-field' value={text_value} onChange={(e) => {setText(e.target.value)}} />
 
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,29 +1,60 @@
 import FileButton from "./FileButton";
+import FunctionButton from "./FunctionButton";
+
 import folder_open from '../assets/folder-open.svg'
 import floppy_disk from '../assets/floppy-disk.svg'
 import plus_solid from '../assets/plus-solid.svg'
 
-function Sidebar(){
+import { open } from '@tauri-apps/api/dialog';
+import {readDir} from '@tauri-apps/api/fs'
+import { useState } from "react";
+
+interface Props{
+    onFileSelect : Function
+    onSave : Function
+}
+
+function Sidebar({onFileSelect, onSave} : Props){
+
+    const [cur_dir, setCurDir] = useState<string>("Select Directory")
+    const [files_list, setFileList] = useState<string[]>([]);
+
+    async function HandleDirectoryChange(){
+        try {
+            console.log("thing")
+            const selectedPath = await open({
+                directory: true,
+                multiple: false,
+                title:"Open Directory"
+            }) as string;
+
+            if(!selectedPath) return;
+
+            setCurDir(selectedPath);
+
+            const files : string[] = (await readDir(selectedPath)).map(entry => entry.name).filter(file => file?.indexOf(".") !== -1) as string[]
+
+            setFileList(files);
+        } catch (error) {
+            console.error(error);
+        }
+    }
 
     return (
         <div className="sidebar">
             <div className="m-4 mb-2 flex flex-row flex-shrink justify-center gap-2 h-16">
-                <button className="function-select"><img className="function-images" src={plus_solid} alt="new"/></button>
-                <button className="function-select"><img className="function-images" src={floppy_disk} alt="save"/></button>
-                <button className="function-select"><img className="function-images" src={folder_open} alt="open"/></button>
+                <FunctionButton src={floppy_disk} alt="Save" onClick={() => {onSave()}} />
+                <FunctionButton src={plus_solid} alt="New" />
+                <FunctionButton src={folder_open} alt="Open" onClick={HandleDirectoryChange} />
             </div>
-            
             
             <div className="bg-gray-600 shadow-md h-fit mx-4 rounded-t-lg">
-                <h1 className="directory-label">Directory Here</h1>
+                <h1 className="directory-label">{cur_dir}</h1>
             </div>
 
-            <div className="bg-gray-600 shadow-md h-full mx-4 rounded-b-lg mb-5 overflow-y-auto ">
+            <div className="bg-gray-600 shadow-md h-full mx-4 rounded-b-lg mb-5 overflow-y-auto overflow-x-clip">
                 <ul>
-                    <li> <FileButton file="test.txt" onFileSelect={(f : string) => console.log(f)}/> </li>
-                    <li> <FileButton file="not_a_text_file.txt" onFileSelect={(f : string) => console.log(f)}/> </li>
-                    <li> <FileButton file="shwartzs_lost_script.txt" onFileSelect={(f : string) => console.log(f)}/> </li>
-
+                    {files_list.map((file : string) => <li key={file}> <FileButton file={file} onFileSelect={(f : string) => {onFileSelect(f, cur_dir)}}/> </li>)}
                 </ul>
             </div>
 
